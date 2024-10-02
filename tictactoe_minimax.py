ad067f793ebdc516e13b889082dd1decb4a60b4b
sidhu <zurajanaikatsurada1222@gmail.com>
2024-10-02 20:10:53 +0530

    Add graph feature in tictactoe

--- tictactoe_minimax.py
+++ tictactoe_minimax.py
@@ -1,4 +1,6 @@
 from copy import deepcopy
+import math
+import matplotlib.pyplot as plt
 
 class TikTacToeState:
     def __init__(self, board_state, parent=None, parent_action=None):
@@ -124,7 +126,13 @@ class PlayTicTacToe:
         self.player1 = player1
         self.player2 = player2
     
-    def play(self):
+    def play(self, terminal=True):
+        if terminal:
+            self._play_terminal()
+        else:
+            self._play_graph()
+
+    def _play_terminal(self):
         state = TikTacToeState([[0, 0, 0], [0, 0, 0], [0, 0, 0]])
         while True:
             if TikTacToeState._terminal_test(state, -1):
@@ -138,9 +146,107 @@ class PlayTicTacToe:
                 break
             state = self.player2.get_next_move(state, -1)
 
+    def _initial_plot(self):
+        self.fig, self.ax = plt.subplots()
+        self.ax.set_xlim(0, 3)
+        self.ax.set_ylim(0, 3)
+        self.ax.set_aspect('equal')
+        for i in range(1, 3):
+            self.ax.axhline(i, color='black', linewidth=2)
+            self.ax.axvline(i, color='black', linewidth=2)
+        self.ax.set_xticks([])
+        self.ax.set_yticks([])
+        plt.pause(0.1)
+
+    def _update_plot(self, state):
+        self.ax.clear()
+        self.ax.set_xlim(0, 3)
+        self.ax.set_ylim(0, 3)
+        self.ax.set_aspect('equal')
+        self.ax.axis(False)
+        for i in range(1, 3):
+            self.ax.axhline(i, color='black', linewidth=2)
+            self.ax.axvline(i, color='black', linewidth=2)
+        for i in range(3):
+            for j in range(3):
+                if state.board_state[i][j] == 1:
+                    self.ax.text(j+0.5, 3-(i+0.5), f'X', ha='center', fontsize=50, va='center', color='red')
+                elif state.board_state[i][j] == -1:
+                    self.ax.text(j+0.5, 3-(i+0.5), f'O', ha='center', fontsize=50, va='center', color='blue')
+        self.ax.set_title("Tic-Tac-Toe")
+        self.state = state
+        plt.pause(0.5)
+    
+    def _on_click(self, event):
+        if self.human_move:
+            row = math.floor(3 - event.ydata)
+            col = math.floor(event.xdata)
+            if self.state.board_state[row][col] == 0:
+                self.state.board_state[row][col] = 1 if self.human_first else -1
+                self.fig.canvas.mpl_disconnect(self.cid)
+                self.human_move = False
+
+    def _final_plot(self, state, result):
+        self._update_plot(state)
+        self.ax.set_title(result)
+        if "Human" in result:
+            self.fig.set_facecolor("g")
+        elif "Computer" in result:
+            self.fig.set_facecolor("b")
+        else:
+            self.fig.set_facecolor("g")
+        plt.pause(0.5)
+
+    def _play_graph(self):
+        state = TikTacToeState([[0, 0, 0], [0, 0, 0], [0, 0, 0]])
+        if isinstance(self.player1, HumanPlayer) and isinstance(self.player2, HumanPlayer):
+            raise Exception("Both players are human")
+        elif isinstance(self.player1, HumanPlayer):
+            self.human_move = True
+            self.human_first = True
+        elif isinstance(self.player2, HumanPlayer):
+            self.human_move = False
+            self.human_first = False
+        else:
+            raise Exception("Human player not found")
+        self._initial_plot()
+        if self.human_move:
+            while True:
+                self._update_plot(state)
+                if TikTacToeState._terminal_test(state, -1):
+                    self._final_plot(state, f"\nGame Over {'Computer won' if self.state.utility_value == -1 else 'draw'}")
+                    break
+                self.cid = self.fig.canvas.mpl_connect('button_press_event', self._on_click)
+                state = self.state
+                self._update_plot(state)
+                if TikTacToeState._terminal_test(state, 1):
+                    self._final_plot(state, f"\nGame Over {'Human won' if self.state.utility_value == 1 else 'draw'}")
+                    break
+                if self.human_move is False:
+                    state = self.player2.get_next_move(self.state, -1)
+                    self.human_move = True
+        else:
+            while True:
+                self._update_plot(state)
+                if TikTacToeState._terminal_test(state, -1):
+                    self._final_plot(state, f"\nGame Over {'Human won' if self.state.utility_value == -1 else 'draw'}")
+                    break
+                if self.human_move is False:
+                    state = self.player1.get_next_move(self.state, 1)
+                    self.human_move = True
+                self._update_plot(state)
+                if TikTacToeState._terminal_test(state, 1):
+                    self._final_plot(state, f"\nGame Over {'Computer won' if self.state.utility_value == 1 else 'draw'}")
+                    break
+                self.cid = self.fig.canvas.mpl_connect('button_press_event', self._on_click)
+                print(self.state.parent_action)
+                state = self.state
+        plt.show()
+
+
     
 if __name__ == "__main__":
     state = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
     root = TikTacToeState(state)
     play = PlayTicTacToe(HumanPlayer(), TikTacToeMinimaxSolver())
-    play.play()
+    play.play(terminal=False)
